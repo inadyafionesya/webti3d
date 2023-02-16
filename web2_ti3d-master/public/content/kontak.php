b590aa709ba8771848d08377313ec2dcc9c729cc
inadyafionesya <125519555+inadyafionesya@users.noreply.github.com>
2023-02-16 09:35:25 +0900

    Add files via upload

---
+++ web2_ti3d-master/public/content/kontak.php
@@ -0,0 +1,26 @@
+<div class="row mt-2">
+    <div class="col-8 mx-auto">
+        <div class="card">
+            <div class="card-body">
+        <h2>Kontak kami</h2>
+        <form action="">
+            <div class="form-floating mb-3">
+                <input tpe="text" class="form-control" placeholder="Masukan Nama Anda">
+                <label for="">Nama</label>
+             </div>
+             <div class="form-floating mb-3">
+                <input type="text" class="form-control"placeholder="Masukan Nama Anda">
+                <label for="">Email</label>
+</div>
+<div class="form-floating mb-3">
+    <textarea name="" class="form-control"rows="5"placeholder="Masukan Pesan"></textarea>
+    <label for="">Pesan</label>
+</div>
+<div class="mb-3">
+    <button class="btn btn-primary"><i class="bibi-send"></i>kirim</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
